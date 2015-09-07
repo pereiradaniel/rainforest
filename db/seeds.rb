c575766f4985f4ff22a3baa81bebcfa86ad5e9cb
Daniel Pereira <pereiradpg@yahoo.ca>
2015-09-07 18:42:06 -0400

    Root goes to index.  rake db:seed performed, and index page shows all items in databse.  Index is root.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!(
+  :name => "A thing.",
+  :description => "Does stuff.",
+  :price_in_cents => 100
+)
+
+Product.create!(
+  :name => "Something else.",
+  :description => "Does other stuff.",
+  :price_in_cents => 500
+)
+
+Product.create!(
+  :name => "Another thing.",
+  :description => "Does more stuff.",
+  :price_in_cents => 800
+)
